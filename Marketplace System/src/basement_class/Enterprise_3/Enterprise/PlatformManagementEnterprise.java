b60754b957197e7ae7271221ef30d88a15863793
bogdog2b <120654221@qq.com>
2025-12-01 15:57:55 -0500

    complete enterprise
    complete strucre

---
+++ Marketplace System/src/basement_class/Enterprise_3/Enterprise/PlatformManagementEnterprise.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basement_class.Enterprise_3.Enterprise;
+
+import basement_class.Enterprise;
+import basement_class.Enterprise_3.Organization.ContentControlOrganization;
+import basement_class.Enterprise_3.Organization.UserControlOrganization;
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class PlatformManagementEnterprise extends Enterprise {
+    
+    public PlatformManagementEnterprise() {
+        super("Platform Management");
+        this.addOrganization(new UserControlOrganization());
+        this.addOrganization(new ContentControlOrganization());
+    }
+    
+    
+
+}
