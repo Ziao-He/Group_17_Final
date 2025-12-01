9edb27f7ae60d331de63a6a3ec169b200c12905b
bogdog2b <120654221@qq.com>
2025-12-01 16:27:06 -0500

    Complete all E3 service

---
+++ Marketplace System/src/service/E3/PolicyEnforcementService.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service.E3;
+
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class PolicyEnforcementService {
+    
+    public void handleViolation(ViolationInvestigationRequest req, boolean banUser) {
+
+        if (banUser) {
+            req.getTargetUser().setStatus("Banned");
+            req.setStatus("User Banned");
+        } else {
+            req.setStatus("Warning Issued");
+        }
+
+        req.resolve();
+    }
+}
