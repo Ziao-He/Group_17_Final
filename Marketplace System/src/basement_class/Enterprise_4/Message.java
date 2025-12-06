9767059b0e8b24f61e5c9c72a4c7364ee9377e68
YujieLiang02 <liang.yuji@northeastern.edu>
2025-12-06 00:05:01 -0500

    add message directory and buyer seller chat ui

--- Marketplace System/src/basement_class/Enterprise_4/Message.java
+++ Marketplace System/src/basement_class/Enterprise_4/Message.java
@@ -78,6 +78,17 @@ public class Message extends BaseEntity{
         this.flaggedByUser = flaggedByUser;
     }
     
-    
+
+    @Override
+    public String toString() {
+        String senderName = (sender != null) ? sender.getUsername() : "Unknown";
+        
+        String shortContent = content;
+        if (shortContent != null && shortContent.length() > 40) {
+            shortContent = shortContent.substring(0, 37) + "...";
+        }
+        return senderName + ": " + shortContent;
+    }
+
 }
 
